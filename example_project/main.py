3ca15a7205b41f14d09439e5ae2e47ab2bcce71a
Saatvik-Aggarwal <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2022-01-06 12:32:22 -0800

    Initial commit

---
+++ example_project/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Use the following links to access the documentation
+# How to control the EV3: https://docs.pybricks.com/en/v2.0/hubs.html#pybricks.hubs.EV3Brick
+# How to control the robot: https://docs.pybricks.com/en/v2.0/ev3devices.html
+# How to implement timing and logging in your program: https://pybricks.com/ev3-micropython/tools.html
+# Rule book: https://firstinspiresst01.blob.core.windows.net/first-forward/fll-challenge/fll-challenge-cargo-connect-robot-game-rulebook.pdf
+
+# --- Begin Example Program ---
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Initialize the drive base. 
+# MIGHT WANT TO CHECK TO MAKE SURE THIS IS RIGHT
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Go forward and backwards for one meter.
+robot.straight(1000)
+ev3.speaker.beep()
+
+robot.straight(-1000)
+ev3.speaker.beep()
+
+# Turn clockwise by 360 degrees and back again.
+robot.turn(360)
+ev3.speaker.beep()
+
+robot.turn(-360)
+ev3.speaker.beep()
+
+# --- End Example Program ---
